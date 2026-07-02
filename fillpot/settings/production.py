261e26dfc03f63a751b72d775ed5d593100e299a
CaptainVee <captainvee3@gmail.com>
2026-07-02 16:35:45 +0100

    pushing to prod

--- fillpot/settings/production.py
+++ fillpot/settings/production.py
@@ -10,12 +10,19 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ["DATABASE_URL"],
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     )
+# }
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ["DATABASE_URL"],
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 MIDDLEWARE = [
